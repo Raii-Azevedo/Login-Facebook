c7bd8c64cdcb6999dd7774b250d6b5dd2b6b0c4c
Raissa Azevedo <rhaii.azevedo@gmail.com>
2021-05-26 12:49:04 -0300

    Debug

--- dsocial/settings.py
+++ dsocial/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4uf!z8c=-5+(k_3(&bd&^ff9cg-!u7q&h@+p_nu$alue!l+m4&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
